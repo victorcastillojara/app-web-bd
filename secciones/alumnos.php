7e2a2439a0521d164092447bc579cc43208e3432
Victor Castillo Jara <victorcastillo.and1@gmail.com>
2024-10-03 15:16:19 -0300

    crud
    se agrega borrar y editar a las vistas alumnos

--- secciones/alumnos.php
+++ secciones/alumnos.php
@@ -3,10 +3,118 @@ include('../configuraciones/bd.php');
 
 $conexionBD = BD::crearInstacia();
 
-$sql="SELECT * FROM alumnos";
 
-$listaAlumnos=$conexionBD->query($sql);
-$alumnos=$listaAlumnos->fetchall();
+//print_r($_POST);
 
+//condicional ternario
+$id = isset($_POST['id']) ? $_POST['id'] : '';
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : '';
+$cursos = isset($_POST['cursos']) ? $_POST['cursos'] : '';
+$accion = isset($_POST['accion']) ? $_POST['accion'] : '';
+
+//transforma todo el nombre a mayuscula
+$nombreM = strtoupper($nombre);
+$apellidoM = strtoupper($apellidos);
+
+if ($accion != '') {
+    switch ($accion) {
+        case 'agregar':
+            $sql = "INSERT INTO alumnos (id, nombre,apellido) VALUES (NULL, :nombre,:apellidos)";
+            $consulta = $conexionBD->prepare($sql);
+            $consulta->bindParam(':nombre', $nombreM);
+            $consulta->bindParam(':apellidos', $apellidoM);
+            $consulta->execute();
+
+            //recupera el ultimo id insertado
+            $idAlumno = $conexionBD->lastInsertId();
+            //echo $sql;
+
+            foreach ($cursos as $curso) {
+                $sql = "INSERT INTO alumnos_cursos (id, idalumno,idcurso) VALUES (NULL, :idalumno,:idcurso)";
+                $consulta = $conexionBD->prepare($sql);
+                $consulta->bindParam(':idalumno', $idAlumno);
+                $consulta->bindParam(':idcurso', $curso);
+                $consulta->execute();
+            }
+            $arregloCursos = $cursos;
+
+            break;
+        case 'editar':
+            $sql = "UPDATE alumnos SET nombre=:nombre,apellido=:apellido WHERE id=:id";
+            $consulta = $conexionBD->prepare($sql);
+            $consulta->bindParam(':id', $id);
+            $consulta->bindParam(':apellido', $apellidoM);
+            $consulta->bindParam(':nombre', $nombreM);
+            $consulta->execute();
+
+            if (isset($cursos)) {
+                $sql = "DELETE FROM alumnos_cursos WHERE idalumno = :idalumno";
+                $consulta = $conexionBD->prepare($sql);
+                $consulta->bindParam(':idalumno', $id);
+                $consulta->execute();
+            }
+
+            foreach ($cursos as $curso) {
+                $sql = "INSERT INTO alumnos_cursos (id,idalumno,idcurso) VALUES (NULL,:idalumno,:idcurso)";
+                $consulta = $conexionBD->prepare($sql);
+                $consulta->bindParam(':idalumno', $id);
+                $consulta->bindParam(':idcurso', $curso);
+                $consulta->execute();
+            }
+
+            break;
+        case 'borrar':
+            $sql = "DELETE FROM alumnos WHERE id=:id";
+            $consulta = $conexionBD->prepare($sql);
+            $consulta->bindParam(':id', $id);
+            $consulta->execute();
+            break;
+        case 'seleccionar':
+            $sql = "SELECT * FROM alumnos WHERE id=:id";
+            $consulta = $conexionBD->prepare($sql);
+            $consulta->bindParam(':id', $id);
+            $consulta->execute();
+            $alumnos = $consulta->fetch(PDO::FETCH_ASSOC);
+            $nombre = $alumnos['nombre'];
+            $apellidos = $alumnos['apellido'];
+            //echo $alumnos['id'];
+
+            $sql = "SELECT cursos.id FROM alumnos_cursos
+            INNER JOIN cursos ON cursos.id=alumnos_cursos.idcurso
+            WHERE alumnos_cursos.idalumno=:idalumno";
+            $consulta = $conexionBD->prepare($sql);
+            $consulta->bindParam(':idalumno', $id);
+            $consulta->execute();
+            $cursosAlumnos = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            // print_r($cursosAlumnos);
+
+            foreach ($cursosAlumnos as $curso) {
+                $arregloCursos[] = $curso['id'];
+            }
+
+            break;
+    }
+}
+
+
+$sql = "SELECT * FROM alumnos";
+$listaAlumnos = $conexionBD->query($sql);
+$alumnos = $listaAlumnos->fetchall();
+
+foreach ($alumnos as $clave => $alumno) {
+    $sql = "SELECT * FROM cursos WHERE id IN(SELECT idcurso FROM alumnos_cursos WHERE idalumno=:idalumno)";
+    $consulta = $conexionBD->prepare($sql);
+    $consulta->bindParam(':idalumno', $alumno['id']);
+    $consulta->execute();
+    $cursosAlumno = $consulta->fetchAll();
+    $alumnos[$clave]['cursos'] = $cursosAlumno;
+}
 //print_r($alumnos);
-?>
+
+
+$sql = "SELECT * FROM cursos";
+$listaCursos = $conexionBD->query($sql);
+$cursos = $listaCursos->fetchAll();
+
+//print_r($cursos);
--- secciones/vista_alumnos.php
+++ secciones/vista_alumnos.php
@@ -16,6 +16,7 @@
                                 class="form-control"
                                 name="id"
                                 id="id"
+                                value="<?php echo $id; ?>"
                                 aria-describedby="helpId"
                                 placeholder="ID" />
 
@@ -27,6 +28,7 @@
                                 class="form-control"
                                 name="nombre"
                                 id="nombre"
+                                value="<?php echo $nombre; ?>"
                                 aria-describedby="helpId"
                                 placeholder="nombre" />
                         </div>
@@ -37,6 +39,7 @@
                                 class="form-control"
                                 name="apellidos"
                                 id="apellidos"
+                                value="<?php echo $apellidos; ?>"
                                 aria-describedby="helpId"
                                 placeholder="apellidos" />
                         </div>
@@ -47,9 +50,21 @@
                                 class="form-select form-select-lg"
                                 name="cursos[]"
                                 id="listaCursos">
-                                <option >Selecciona una opcion</option>
-                                <option value="Curso 1">Curso 1</option>
-                                <option value="Curso 2">Curso 2</option>
+                                <option disabled>Selecciona una opcion</option>
+                                <?php foreach ($cursos as  $curso) { ?>
+                                    <option
+                                        <?php
+                                        if (!empty($arregloCursos)):
+                                            if (in_array($curso['id'], $arregloCursos)):
+                                                echo "selected";
+                                            endif;
+
+                                        endif;
+                                        ?>
+                                        value=<?php echo $curso['id'];  ?>>
+                                        <?php echo $curso['id']; ?> - <?php echo $curso['nombre_curso']; ?>
+                                    </option>
+                                <?php } ?>
                             </select>
                         </div>
                         <div class="btn-group" role="group" aria-label="Button group name">
@@ -84,9 +99,9 @@
 
             </form>
         </div>
-        <div class="col-7">
+        <div class="col-md-7">
             <div class="table-responsive">
-                <table class="table ">
+                <table class="table table-primary">
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -95,12 +110,27 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($alumnos as $alumno) {?>
-                        <tr >
-                            <td><?php echo $alumno['id'] ?></td>
-                            <td><?php echo $alumno['nombre'].' '.$alumno['apellido']?></td>
-                            <td></td>
-                        </tr>
+                        <?php foreach ($alumnos as $alumno) { ?>
+                            <tr>
+                                <td><?php echo $alumno['id'] ?></td>
+                                <td>
+                                    <?php echo $alumno['nombre'] . ' ' . $alumno['apellido'] ?><br>
+                                    <?php //print_r($alumno['cursos']); 
+
+                                    foreach ($alumno['cursos'] as $curso) { ?>
+                                        - <a href="certificado.php?idcurso=<?php echo $curso['id'] ?>&idalumno=<?php echo $alumno['id'] ?>"> <?php echo $curso['nombre_curso']; ?></a> <br>
+                                    <?php } ?>
+
+
+                                </td>
+                                <td>
+                                    <form action="" method="post">
+                                        <input type="hidden" name="id" id="id" value="<?php echo $alumno['id']; ?>">
+                                        <input type="submit" class="btn btn-info" name="accion" value="seleccionar">
+                                    </form>
+
+                                </td>
+                            </tr>
                         <?php    }    ?>
                     </tbody>
                 </table>
@@ -108,4 +138,12 @@
         </div>
     </div>
 </div>
+
+
+<link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
+
+<script>
+    new TomSelect('#listaCursos');
+</script>
 <?php include('../templates/pie.php'); ?>
